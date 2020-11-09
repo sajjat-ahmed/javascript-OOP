4256d5089ac81c4e71714f916153225e995a4100
sajjat-ahmed <ahm.sajjat@gmail.com>
2020-11-09 15:29:30 +0600

    oop done

--- prototype.js
+++ prototype.js
@@ -9,3 +9,14 @@ Cat.prototype.talk = function() {
 const myCat = new Cat('Tom');
 console.log(myCat);
 myCat.talk();
+
+
+// npx create-react-app ReactApp
+// npm react-native init ReactNativeApp
+// npm install @material-ui/core @material-ui/icons
+// npm install firebase firebase-tools firebase-hooks
+// npm init --y | npm install express mongodb cors body-parser
+// yarn add firebase firebase-tools firebase-hooks
+// npm init --y
+// OOP => Object Oriented Programming
+// Data Structure and Algorithon
